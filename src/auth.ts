0ed5afcc97e04df568cf22d9d781c15264fb2aba
liuzeqi <liuzeqi@yiwise.com>
2024-12-09 10:39:24 +0800

    chore: 使用next-auth实现谷歌登录

---
+++ src/auth.ts
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  secret: process.env.NEXT_AUTH_SECRET,
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token }) {
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user && token?.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+});
