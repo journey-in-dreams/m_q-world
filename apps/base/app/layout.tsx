35ca77309f83ec11e9eaf0cf69837b1561e93c85
liuzeqi <liuzeqi@yiwise.com>
2025-10-11 11:03:04 +0800

    feat: 调整目录结构,utils函数全部放在infra文件夹下

--- apps/base/app/layout.tsx
+++ apps/base/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+// import { Button } from "@wq/shadcn/button"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        {/* <Button>12</Button> */}
       </body>
     </html>
   );
