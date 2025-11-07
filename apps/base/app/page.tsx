a25d61c100e8ebc672a6d42dad5b5828cd5ef72f
liuzeqi <liuzeqi@yiwise.com>
2025-11-07 17:13:37 +0800

    feat: 调整目录结构

---
+++ apps/base/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@mq/ui/button';
+import styles from './page.module.css';
+
+export default function Home() {
+	return (
+		<div className={styles.page}>
+			<Button>测试按钮</Button>
+		</div>
+	);
+}
